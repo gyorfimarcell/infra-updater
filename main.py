ed257eacf342a3db56244a43f3f8e903ca3c90ed
Györfi Marcell <gyorfimarcell@gmail.com>
2025-09-24 16:55:12 +0200

    Add docker and route

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from update_infra import update_infra
+from pydantic import BaseModel
+from settings import settings
 
 app = FastAPI()
 
 
-@app.get("/")
-async def update():
+class UpdateBody(BaseModel):
+    secret: str
+
+
+@app.post("/update")
+async def update(body: UpdateBody):
+    if body.secret != settings.webhook_secret:
+        raise HTTPException(status_code=403)
+
     update_infra()
 
-    return "Hello world"
+    return "Done"
--- settings.py
+++ settings.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     config_path: str = "./config/main"
     gitea_token: str = ""
     competitors_compose_path: str = "./competitors.yml"
+    webhook_secret: str = "secret"
 
     model_config = SettingsConfigDict(env_file=".env")
 
