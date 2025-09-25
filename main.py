2424a33259319787755be23ffdb67dbd1aa739b9
Györfi Marcell <gyorfimarcell@gmail.com>
2025-09-25 09:22:05 +0200

    Add signature verification

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
-from fastapi import FastAPI, HTTPException
-from update_infra import update_infra
-from pydantic import BaseModel
+import hashlib
+import hmac
+
+from fastapi import FastAPI, Header, HTTPException, Request, status
+
 from settings import settings
+from update_infra import update_infra
 
 app = FastAPI()
 
 
-# class UpdateBody(BaseModel):
-#     secret: str
-
-
 @app.post("/update")
-async def update():  # body: UpdateBody):
-    # if body.secret != settings.webhook_secret:
-    #     raise HTTPException(status_code=403)
+async def update(request: Request, x_gitea_signature: str = Header(None)):
+    body = await request.body()
+
+    if not x_gitea_signature:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Missing X-Gitea-Signature header",
+        )
+
+    computed_signature = hmac.new(
+        key=settings.webhook_secret.encode(), msg=body, digestmod=hashlib.sha256
+    ).hexdigest()
+
+    if not hmac.compare_digest(computed_signature, x_gitea_signature):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Invalid signature"
+        )
 
     update_infra()
 
